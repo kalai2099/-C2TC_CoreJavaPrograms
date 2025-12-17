f46bce1e1823bb7b5ba2a8bdb1fe0d4a0ec64971
Tharun <Tharun@DESKTOP-92IHA2B>
2025-12-17 20:11:07 +0530

    initial

---
+++ Javaproject1/src/javapro/Breakdemo6.java
@@ -0,0 +1,15 @@
+package javapro;
+
+public class Breakdemo6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 2; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
+
+	}
+
+}
